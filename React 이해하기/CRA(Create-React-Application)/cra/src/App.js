e976533b9703567ff8c1860644437aeedfc8431c
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-27 21:59:15 +0900

    Lifecycle 클래스형 컴포넌트 실습 해당 학습 내용 추가

--- React 이해하기/CRA(Create-React-Application)/cra/src/App.js
+++ React 이해하기/CRA(Create-React-Application)/cra/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 // import Counter from "./components/counter";
 // import Counter2 from "./components/counter2";
-import Calculator from "./components/stateUp/Calculator";
+// import Calculator from "./components/stateUp/Calculator";
+import Clock from "./components/clock(lifecycle)/clock";
 
 function App() {
-  return <Calculator />;
+  return <Clock />;
 }
 
 export default App;
