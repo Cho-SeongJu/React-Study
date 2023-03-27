153ff980329bb422bd0ef94a7f0a1976db031db0
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-27 15:19:35 +0900

    jsx 문법 실습 jsx.txt -> jsx 학습 내용 파일
    cra 폴더에서 실습

--- React 이해하기/CRA(Create-React-Application)/cra/src/App.js
+++ React 이해하기/CRA(Create-React-Application)/cra/src/App.js
@@ -2,17 +2,30 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const message = "Learn React";
+  // const getGreetingMessage = (name) => {
+  //   if (name === "hwarari") return `Hello, ${name}! Learn React`;
+  //   return `Welcome, ${name}! Learn React`;
+  // };
+
+  const name = "hwarari";
+  const count = 0;
+  const memo = "";
   return (
-    <div className="App">
+    <div className="App" tabIndex="0">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Yahoo !!!!
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+          {name === "hwarari" && `Hello, ${name}! Learn React`}
+          <div>{!!count && <h1>Messages: {count}</h1>}</div>
         </a>
+        {memo !== null && `메모:${memo} `}
       </header>
+      <input type="checkbox" name="agreement" id="agreement-el"></input>
+      <label htmlFor="agreement-el">동의</label>
     </div>
   );
 }
