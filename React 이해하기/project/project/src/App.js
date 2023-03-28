51d640c50f5d9802569b387e56df2609976a1707
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-29 00:18:38 +0900

    Nested Routes(중첩된 Routes) 구성하기

--- React 이해하기/project/project/src/App.js
+++ React 이해하기/project/project/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./blog/components/MainPage";
 import TechPage from "./blog/components/TechPage";
 import BlogPage from "./blog/components/BlogPage";
+import JavaScriptPage from "./blog/components/JavaScriptPage";
+import ReactPage from "./blog/components/ReactPage";
 
 // root url : main page component
 // tech url : tech page component
@@ -13,7 +15,10 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<MainPage />}></Route>
-        <Route path={"/tech"} element={<TechPage />}></Route>
+        <Route path={"/tech"} element={<TechPage />}>
+          <Route path="javascript" element={<JavaScriptPage />} />
+          <Route path="react" element={<ReactPage />} />
+        </Route>
         <Route path={"/blog"} element={<BlogPage />}></Route>
       </Routes>
     </BrowserRouter>
--- React 이해하기/project/project/src/blog/components/TechPage.js
+++ React 이해하기/project/project/src/blog/components/TechPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 export default function TechPage() {
   return (
-    <div>
-      <h1>TechPage</h1>
-    </div>
+    <>
+      <div>
+        <h1>TechPage</h1>
+        <Link to="/tect/react">React</Link> |<Link to="/tect/javascript">Javascript</Link>
+      </div>
+      <Outlet />
+    </>
   );
 }
