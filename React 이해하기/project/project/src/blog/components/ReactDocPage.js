934143b0333cead77f1b4c476c6507a5ab87619b
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-29 00:46:42 +0900

    useNavigate 사용
    useNavigate는 페이지를 이동할 때 사용된다.
    Link를 써도 페이지 이동을 시킬 수 있지만 단순하게 이동만 시켜야 하는 경우 Link를 사용하면 좋고
    useNavigate를 사용하면 특정 이벤트가 실행됐을 때 동작하도록 하거나 추가적인 로직이 필요한 경우 useNavigate를 사용한다.

--- React 이해하기/project/project/src/blog/components/ReactDocPage.js
+++ React 이해하기/project/project/src/blog/components/ReactDocPage.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function ReactDocPage() {
   // docID : 1, 2, 3 ...
-
+  const navigate = useNavigate();
   const params = useParams();
-  return <div>ReactDocPage ###{params.docId}</div>;
+  return (
+    <>
+      <h5 onClick={() => navigate("/")}>logo</h5>
+      <div>ReactDocPage ###{params.docId}</div>
+    </>
+  );
 }
