da313e46ba92fad9f0419461e9c2593ebb414162
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-28 23:17:50 +0900

    컴포넌트 스타일링 css-in-js styled components 라이브러리 추가 styled components로 스타일링한 코드 추가

--- React 이해하기/project/project/src/mockupData/components/ProductsTable.js
+++ React 이해하기/project/project/src/mockupData/components/ProductsTable.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import * as S from "./ProductsTable.style";
+import styled from "styled-components";
 
 export default function ProductsTable(props) {
   const { category, items, inStockOnly } = props;
@@ -8,14 +10,22 @@ export default function ProductsTable(props) {
   return (
     <>
       <tr>
-        <td style={{ fontWeight: "bold" }}>{category}</td>
+        <Category>{category}</Category>
       </tr>
       {flteredItems.map((item, idx) => (
         <tr key={idx}>
-          <td style={{ color: item.stocked ? "black" : "red" }}>{item.name}</td>
+          <ProductName stocked={item.stocked}>{item.name}</ProductName>
           <td>{item.price}</td>
         </tr>
       ))}
     </>
   );
 }
+
+const Category = styled.td`
+  font-weight: bold;
+`;
+
+const ProductName = styled.td`
+  color: ${(props) => (props.stocked ? "black" : "red")};
+`;
