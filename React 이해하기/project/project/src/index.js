4365045162f6f2143b6dc930c9818742f8a82225
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-28 14:35:18 +0900

    클래스 컴포넌트에서 이벤트 처리하기

--- React 이해하기/project/project/src/index.js
+++ React 이해하기/project/project/src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from './App';
-import Banner from "./banner/Banner";
+// import Banner from "./banner/Banner";
+import Toggle from "./toggle";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Banner />
+    <Toggle />
   </React.StrictMode>
 );
 
