4d06f23878f238c3b72f2bf3e23520731b776e7f
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-29 00:01:12 +0900

    SPA 학습내용, react-routing-dom 추가

--- React 이해하기/project/project/src/App.js
+++ React 이해하기/project/project/src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
-import OnlineStore from "./mockupData/components/OnlineStore";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./blog/components/MainPage";
+import TechPage from "./blog/components/TechPage";
+import BlogPage from "./blog/components/BlogPage";
+
+// root url : main page component
+// tech url : tech page component
+// blog url : blog page component
 
 export default function App() {
-  return <OnlineStore />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<MainPage />}></Route>
+        <Route path={"/tech"} element={<TechPage />}></Route>
+        <Route path={"/blog"} element={<BlogPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ React 이해하기/project/project/src/mockupData/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import OnlineStore from "./mockupData/components/OnlineStore";
+
+export default function App() {
+  return <OnlineStore />;
+}
