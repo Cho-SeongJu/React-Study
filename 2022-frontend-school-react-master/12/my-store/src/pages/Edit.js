203ca3a499a48b754fd41b76d5c64ef415461b53
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-30 03:07:54 +0900

    React-query
    suspense 추가

--- 2022-frontend-school-react-master/12/my-store/src/pages/Edit.js
+++ 2022-frontend-school-react-master/12/my-store/src/pages/Edit.js
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
-import { useQuery, useMutation, useQueryClient } from "react-query";
-import { getUser, updateNickname } from "../mocks/api";
+import { useQuery, useQueries, useMutation, useQueryClient } from "react-query";
+import { getUser, getPosts, updateNickname } from "../mocks/api";
 
 // API를 통해 현재 닉네임값 가져오기
 // handleSubmit : 업데이트된 inputValue를 서버에 전송해서, 닉네임값 없데이트 하기
 export default function Edit() {
   const [inputValue, setInputValue] = useState("");
   const queryClient = useQueryClient();
-  const { data, isLoading } = useQuery("@getUser", getUser, {
-    staleTime: Infinity,
-  });
+
+  // const { data: user } = useQuery("@getUser", getUser, {
+  //   staleTime: Infinity,
+  //   suspense: true,
+  // });
+
+  // const { data: posts } = useQuery("@getPosts", getPosts, {
+  //   staleTime: Infinity,
+  //   suspense: true,
+  // });
+
+  // useQuery 여러개 배열에 객체로 넣어서 한번에
+  const results = useQueries([
+    {
+      queryKey: ["@getUser"],
+      queryFn: getUser,
+      staleTime: Infinity,
+    },
+    {
+      queryKey: ["@getPosts"],
+      queryFn: getPosts,
+      staleTime: Infinity,
+    },
+  ]);
+
+  console.log(results);
+
+  const user = results[0].data;
+  const posts = results[1].data;
+
   // mutation에 함수를 넣어주고
   const mutation = useMutation(updateNickname, {
     onSuccess: () => {
@@ -26,18 +53,27 @@ export default function Edit() {
     mutation.mutate(inputValue);
   };
 
-  if (isLoading) return <span>Loading...</span>;
+  // if (isLoading) return <span>Loading...</span>;
+
+  // return null;
+
+  // 아직 queries로 는 suspense 처리 오류가 해결되지 않아 일단 옵셔널 체이닝로 하면 오류 해결은 된다.
 
   return (
     <>
       <h1>Edit</h1>
-      <h3>현재 닉네임: {data.nickName}</h3>
+      <h3>현재 닉네임: {user?.nickName}</h3>
       <form onSubmit={handleSubmit}>
         <label>
           변경할 닉네임:
           <input type="text" value={inputValue} onChange={handleChange} />
         </label>
       </form>
+      <ul>
+        {posts?.map((post) => (
+          <li>{post.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- 2022-frontend-school-react-master/12/my-store/src/pages/Home.js
+++ 2022-frontend-school-react-master/12/my-store/src/pages/Home.js
@@ -8,7 +8,11 @@ export default function Home() {
   // statleTime을 Infinity로 하면 cache 되어있는 데이터를 사용한다.
   const { data, isLoading } = useQuery("@getUser", getUser, {
     staleTime: Infinity,
+    suspense: true,
   });
+
+  if (isLoading) return <span>Loading...</span>;
+
   return (
     <div>
       <h1>Hello, {data?.nickName}</h1>
