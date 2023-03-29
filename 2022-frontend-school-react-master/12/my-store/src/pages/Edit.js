f5c1139ad93e29b1bffeb29ee70b87a168b94f4a
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-30 01:35:52 +0900

    React Query
    Mutations
    Query Invalidation
    사용

--- 2022-frontend-school-react-master/12/my-store/src/pages/Edit.js
+++ 2022-frontend-school-react-master/12/my-store/src/pages/Edit.js
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
-import { getUser } from "../mocks/api";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { getUser, updateNickname } from "../mocks/api";
 
 // API를 통해 현재 닉네임값 가져오기
 // handleSubmit : 업데이트된 inputValue를 서버에 전송해서, 닉네임값 없데이트 하기
 export default function Edit() {
   const [inputValue, setInputValue] = useState("");
-
+  const queryClient = useQueryClient();
   const { data, isLoading } = useQuery("@getUser", getUser);
+  // mutation에 함수를 넣어주고
+  const mutation = useMutation(updateNickname, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("@getUser");
+    },
+  });
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    // mutate에 업데이트 할 값을 넣는다.
+    mutation.mutate(inputValue);
   };
 
   if (isLoading) return <span>Loading...</span>;
