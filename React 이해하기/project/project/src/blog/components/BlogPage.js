80a65b64b719b6c4a939eb84881cc94ec0c1cfd6
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-29 22:20:33 +0900

    useReducer 실습 내용 추가

--- React 이해하기/project/project/src/blog/components/BlogPage.js
+++ React 이해하기/project/project/src/blog/components/BlogPage.js
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import { UserContext } from "./store/user";
 
 export default function BlogPage() {
-  const value = useContext(UserContext);
-  console.log(value);
+  // const value = useContext(UserContext);
+  const dispatch = useContext(UserContext);
+  console.log(dispatch);
   return (
     <div>
       <h1>BlogPage</h1>
+      <button onClick={() => dispatch({ type: "changeJob", text: "BE-developer" })}>Change Job</button>
     </div>
   );
 }
--- React 이해하기/project/project/src/blog/components/store/user.js
+++ React 이해하기/project/project/src/blog/components/store/user.js
@@ -1,18 +1,34 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useReducer } from "react";
 //create Context : context를 생성하게 해주는 함수
 
 export const UserContext = createContext();
 
-export default function UserStore(props) {
-  const [job, setJob] = useState("FE-developer");
+const initialUser = {
+  name: "hwarari",
+  job: "FE-developer",
+  // changeJob: (updatedJob) => setJob(updatedJob),
+};
 
-  const user = {
-    name: "hwarari",
-    job,
-    changeJob: (updatedJob) => setJob(updatedJob),
-  };
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case "changeJob":
+      //state의 job에 해당하는 데이터를 action.text
+      return { ...state, job: action.text };
+      break;
+  }
+};
+
+export default function UserStore(props) {
+  // const [job, setJob] = useState("FE-developer");
+  const [user, dispatch] = useReducer(userReducer, initialUser);
+  console.log(user);
+  // const user = {
+  //   name: "hwarari",
+  //   job,
+  //   changeJob: (updatedJob) => setJob(updatedJob),
+  // };
 
   // 하위 컴포넌트에게도 제공한다.
   // value에 담긴 데이터만 넘긴다.
-  return <UserContext.Provider value={user}>{props.children}</UserContext.Provider>;
+  return <UserContext.Provider value={dispatch}>{props.children}</UserContext.Provider>;
 }
