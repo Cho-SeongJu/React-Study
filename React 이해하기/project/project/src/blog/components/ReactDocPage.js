6fec3e548e84b3974ebc3edb51b0fbf7e7fc012d
tjdwn9753 <tjdwn9753@gmail.com>
2023-03-29 20:34:42 +0900

    비동기 프로그래밍 및 API 호출 방법 axios, async / await, useSWR, useFetch, custom Hook

--- React 이해하기/project/project/src/blog/components/ReactDocPage.js
+++ React 이해하기/project/project/src/blog/components/ReactDocPage.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function ReactDocPage() {
   // docID : 1, 2, 3 ...
   const navigate = useNavigate();
   const params = useParams();
+  const [docs, setDocs] = useState({});
+
+  useEffect(() => {
+    const res = async () => {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.docId}`);
+      setDocs(response.data);
+    };
+
+    res();
+  }, []);
+
   return (
     <>
       <h5 onClick={() => navigate("/")}>logo</h5>
       <div>ReactDocPage ###{params.docId}</div>
+      <p>{docs.title === undefined ? " Loading..." : docs.title}</p>
     </>
   );
 }
