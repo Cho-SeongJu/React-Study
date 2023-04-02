b050f8238bdfe90686728f26608f53def0ebe31b
tjdwn9753 <tjdwn9753@gmail.com>
2023-04-02 15:14:10 +0900

    토이 프로젝트 github-issue 페이지
    open/close 필터 추가

--- 여러가지 React 실습/github-issue/src/ListContainer.js
+++ 여러가지 React 실습/github-issue/src/ListContainer.js
@@ -3,6 +3,7 @@ import styles from './ListContainer.module.css';
 import Button from './components/Button';
 import ListItem from './components/ListItem';
 import ListItemLayout from './components/ListItemLayout';
+import cx from 'clsx';
 
 const ListContainer = () => {
   const [inputValue, setInputValue] = useState('is:pr is:open');
@@ -21,6 +22,7 @@ const ListContainer = () => {
         />
         <Button style={{ fontSize: '14px', backgroundColor: 'green', color: 'white' }}>New Issue</Button>
       </div>
+      <OpenClosedFilters />
       <ListItemLayout className={styles.listFilter}>
         <div className={styles.filterList}>
           <span>Author</span>
@@ -42,6 +44,44 @@ const ListContainer = () => {
         />
       </div>
     </div>
+  );
+};
+
+const OpenClosedFilters = ({ data }) => {
+  const [isOpenMode, setIsOpenMode] = useState(true);
+  // const data = getData();
+  // const opendData = data.filter((d) => d.state === 'open')
+  // const closedData = data.filter((d) => d.state === 'closed')
+  const openModeDataSize = 1;
+  const closeModeDataSize = 2;
+
+  return (
+    <>
+      <OpenClosedFilter
+        size={openModeDataSize}
+        state="Open"
+        selected={isOpenMode}
+        onClick={() => setIsOpenMode(true)}
+      />
+      <OpenClosedFilter
+        size={closeModeDataSize}
+        state="Closed"
+        selected={!isOpenMode}
+        onClick={() => setIsOpenMode(false)}
+      />
+    </>
+  );
+};
+
+const OpenClosedFilter = ({ size, state, onClick, selected }) => {
+  return (
+    <span
+      role="button"
+      className={cx(styles.textFilter, { [styles.selected]: selected })}
+      onClick={onClick}
+    >
+      {size} {state}
+    </span>
   );
 };
 
